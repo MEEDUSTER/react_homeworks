b8259cdfe30e2c526f940d74dfc7cb74430ad27a
MEDUSTER <leyplayveb@gmail.com>
2026-09-15 21:59:36 +0300

    HW-8

--- clone-hw.sh
+++ clone-hw.sh
@@ -5,7 +5,7 @@ if ! command -v git &> /dev/null; then
     exit 1
 fi
 
-folders=("hw-1" "hw-2" "hw-3" "hw-4" "hw-5" "hw-6" )
+folders=("hw-1" "hw-2" "hw-3" "hw-4" "hw-5" "hw-6" "hw-7" "hw-8" "hw-9")
 
 select_option() {
     local selected=0
